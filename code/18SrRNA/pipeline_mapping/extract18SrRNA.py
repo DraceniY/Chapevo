3c886c532444c7482d86a79453b74c54ff73a877
pechmannlab <pechmann.udem@gmail.com>
2019-02-04 20:37:39 -0500

    code update

--- code/18SrRNA/pipeline_mapping/extract18SrRNA.py
+++ code/18SrRNA/pipeline_mapping/extract18SrRNA.py
@@ -15,7 +15,7 @@ import subprocess
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def create_index(genome_fasta):
+def create_index(genome_fasta, nameOUT):
     """
     creates HISAT index for target genome
     aligns synthetic rRNA reads
@@ -38,12 +38,15 @@ def create_index(genome_fasta):
     if not os.path.exists('TMP'):
         os.makedirs('TMP')
 
-    cmd_align = './hisat2' + ' ' + '-f' + ' ' + '-a' + ' ' + '-x' + prefix + ' ' + '-U' + ' ' + SYN_rRNA_FASTA + ' ' + '>' + ' ' + 'TMP/' + prefix + '.sam'
+    cmd_align = './hisat2' + ' ' + '-f' + ' ' + '-a' + ' ' + '-x' + prefix + ' ' + '-U' + ' ' + SYN_rRNA_FASTA + ' ' + '>' + ' ' + nameOUT
     subprocess.call(cmd_align, shell=True) 
 
     # remove index
     if os.path.exists(prefix + '*.ht2'): 
         os.remove(prefix + '*.ht2') 
+
+
+
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -63,14 +66,67 @@ def parse_sam_file(sam_file):
                 chromo = current_line[2]
                 pos = int(current_line[3])
                 read= current_line[0]
-                
-                if pos > 0 and chromo != "*":            
+
+                if pos > 0 and chromo != "*":
                     sam_df.loc[counter]= (read , pos , chromo)
                     counter += 1
-                    
+
                     if counter % 1000 == 0:
                         print(counter, "reads read into DF")
     return sam_df
+
+
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def parse_sorted_sam_file(sam_file):
+    """
+    load SAM file into data frame
+    """
+
+    best_df = None
+    best_cov = 0
+    last_chromo = None
+    
+    sam_df = pd.DataFrame(columns=['reads', 'position', 'chromosome'])
+    counter=0
+
+    with open(sam_file, "r") as f:
+
+        for line in f:
+            if line[0] != "@" and line[0] != "/":
+                current_line = line.split()
+                chromo = current_line[2]
+                pos = int(current_line[3])
+                read= current_line[0]
+
+                if chromo == last_chromo: 
+                
+                    if pos > 0 and chromo != "*":            
+                        sam_df.loc[counter]= (read , pos , chromo)
+                        last_chromo = chromo
+                        counter += 1
+                   
+                elif chromo != last_chromo:
+                    if len(sam_df['position']) > best_cov:
+                        best_df = sam_df
+                        best_cov = len(best_df['position'])
+
+                    sam_df = pd.DataFrame(columns=['reads', 'position', 'chromosome'])
+                    counter = 0
+                    sam_df.loc[counter] = (read, pos, chromo)
+                    last_chromo = chromo
+                    counter += 1
+
+    if len(sam_df['position']) > best_cov:
+        best_df = sam_df
+ 
+    return best_df
+
+
 
 
 
@@ -81,17 +137,13 @@ def analyze_sam(sam_df):
     extracts regions of candidate 18S rRNA with best 'coverage'
     """
 
-    list_chromosomes =  set(sam_df["chromosome"])
-
-    if len(list_chromosomes) > 1000:
-        list_chromosomes = list_chromosomes[0:1000]
-
+    list_chromosome = set(sam_df["chromosome"].values)
 
     best_chromosome = "None"
     best_coverage = 0
     best_start = 10000000000
     best_end = 0
-    best_length = -1
+    best_len = -1
 
     nchromo = 0
     for chromo in list_chromosomes:
@@ -117,9 +169,9 @@ def analyze_sam(sam_df):
 
     if best_chromosome != "None" and best_len > 0 and best_len < 3000 :
 
-        # add 100 bases to start and end position
-        start = int(best_start) - np.minimum(100, int(best_start) ) 
-        end   = int(best_end) + 100
+        # add 500 bases to start and end position
+        start = int(best_start) - np.minimum(500, int(best_start) ) 
+        end   = int(best_end) + 500
 
 
     elif best_chromosome != "None" and best_len > 3000:
@@ -150,8 +202,78 @@ def analyze_sam(sam_df):
         start = 'none'
         end   = 'none' 
 
+    print(best_chromosome, start, end)
+    return best_chromosome, start, end
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def analyze_sorted_sam(sam_df):
+    """
+    extracts regions of candidate 18S rRNA with best 'coverage'
+    """
+
+    best_chromosome = list(set(list(sam_df["chromosome"])))
+    best_chromosome =  best_chromosome[0]
+
+
+    best_coverage = 0
+    best_start = 10000000000
+    best_end = 0
+    best_len = -1
+
+
+    # get max , min and length of chromosome
+    current_end   = np.max( np.array(sam_df["position"] ) )
+    current_start = np.min( np.array(sam_df["position"] ) )
+    current_len   = int(current_end) - int(current_start)
+    nb_reads = len(set(sam_df["reads"].tolist()))
+
+    if nb_reads > best_coverage and current_len > 500:
+        best_coverage = np.copy(nb_reads)
+        best_start = np.copy(current_start)
+        best_end  = np.copy(current_end)
+        best_len  = int(best_end) - (best_start)
+
+
+    if best_len > 0 and best_len < 3000 :
+
+        # add 500 bases to start and end position
+        start = int(best_start) - np.minimum(500, int(best_start) )
+        end   = int(best_end) + 500
+
+    elif best_len > 3000:
+        # find a smaller candidate region, here e.g. of max 3000nt
+        current_pos = np.array(sam_df['position'])
+        new_start = best_start
+        new_end = best_end
+        new_coverage = 0
+
+        for i in range(int(best_start), int(best_end) - 3000):
+            sel_lo = current_pos > i
+            sel_hi = current_pos < i + 3000
+            sel = sel_lo * sel_hi
+            no_reads = len(current_pos[sel])
+
+            if no_reads > new_coverage:
+                new_start = i
+                new_end = i + 3000
+                new_coverage = np.copy(no_reads)
+
+        start = new_start
+        end   = new_end
+
+
+    else:
+        best_chromosome = 'none'
+        start = 'none'
+        end   = 'none'
+
 
     return best_chromosome, start, end
+
 
 
 
@@ -163,60 +285,70 @@ def run_pipeline(genome_file):
     """
 
     prefix = genome_file.split("/")[-1].split(".")[0]
+    sam_file = 'TMP/'+prefix+'.sam'
 
     # create index and align rRNA reads
-    if not os.path.exists('TMP/'+prefix+'.sam'):
-        create_index(genome_file)
+    if not os.path.exists(sam_file):
+        create_index(genome_file, sam_file)
 
-    # parse SAM file
-    current_sam = parse_sam_file('TMP/'+prefix+'.sam')
+    if os.path.exists(sam_file) and os.path.getsize(sam_file) > 0:
+
+        cmd_sort = 'samtools sort' + ' ' + sam_file + ' ' + '-o tmp.sam'
+        subprocess.call(cmd_sort, shell=True)
+
+        # parse SAM file
+        #current_sam = parse_sam_file(sam_file)
+        current_sam = parse_sorted_sam_file('tmp.sam')
 
         
-    #print("Identify chromosomal region with highest coverage")
-    chromo, start, end = analyze_sam(current_sam)
+        #print("Identify chromosomal region with highest coverage")
+        #chromo, start, end = analyze_sam(current_sam)
+        chromo, start, end = analyze_sorted_sam(current_sam)
 
-    if chromo != 'none':
+        if chromo != 'none':
 
-        # load genome fasta and extract candidate seq
-        current_fasta = SeqIO.to_dict(SeqIO.parse(genome_file, "fasta"))
-        candidate_seq = current_fasta[chromo][start:end]
-        SeqIO.write(candidate_seq , prefix + "_candidate.fasta" , "fasta")
+            # load genome fasta and extract candidate seq
+            current_fasta = SeqIO.to_dict(SeqIO.parse(genome_file, "fasta"))
+            print(chromo)
+            candidate_seq = current_fasta[chromo][start:end]
+            SeqIO.write(candidate_seq , prefix + "_candidate.fasta" , "fasta")
 
+            if not os.path.exists('result'):
+                os.makedirs('result')
 
-        if not os.path.exists('result'):
-            os.makedirs('result')
+            # run RNAmmer
+            print("Running RNAmmer")
+            if os.path.exists(prefix+"_candidate.fasta"):
 
+                cmd_rnammer = "perl rnammer -S euk -m ssu -gff result/"+prefix+"_gff -h result/"+prefix+"_report -f result/"+prefix+"_18S_output.fasta "+prefix+"_candidate.fasta"
+                subprocess.call(cmd_rnammer, shell=True)
 
-        # run RNAmmer
-        print("Running RNAmmer")
-        if os.path.exists(prefix+"_candidate.fasta"):
+                print("18S rRNA sequence written to file", prefix+"_18S_output.fasta") 
 
-            cmd_rnammer = "perl rnammer -S euk -m ssu -gff result/"+prefix+"_gff -h result/"+prefix+"_report -f result/"+prefix+"_18S_output.fasta "+prefix+"_candidate.fasta"
-            subprocess.call(cmd_rnammer, shell=True)
-
-            print("18S rRNA sequence written to file", prefix+"_18S_output.fasta") 
-
-            if os.path.exists("temp.*"):
-                os.remove("temp.*")
-            if os.path.exists("1000000*"):
-                os.remove("1000000*")
-            if os.path.exists("*.hmmsearchresult"):
-                os.remove("*.hmmsearchresult")
-
+                if os.path.exists("temp.*"):
+                    os.remove("temp.*")
+                if os.path.exists("1000000*"):
+                    os.remove("1000000*")
+                if os.path.exists("*.hmmsearchresult"):
+                    os.remove("*.hmmsearchresult")
 
     else:
         print("no rRNA found")
 
 
+    if os.path.exists('tmp.sam'):
+        os.remove('tmp.sam')
+
+
 if __name__ == '__main__':
 
 
-    list_genomes = glob.glob('genomes/*.fna')
-    #list_genomes = [line.rstrip('\n') for line in open('list_genomes.txt')]
+    #list_genomes = glob.glob('/home/sebastian/Denali/genomes/*.fna')
+    list_genomes = [line.rstrip('\n') for line in open('list_genomes.txt')]
 
     for ix, i in enumerate(list_genomes):
         print("Analyzing", i)
-        run_pipeline(list_genomes[ix])
+        run_pipeline(i)
 
  
         if ix % 10 == 0: 
